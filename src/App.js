ad1fcafde2c59ebf40f4e0132febac977b00cd03
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-18 03:19:44 +0900

    make MovieDetail

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import GlobalStyle from './styles/GlobalStyle';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import MovieList from './components/MovieList';
 import { MovieListProvider } from './contexts/MoveListContext';
-
+import MovieDetail from './components/MovieDetail';
 function App() {
   return (
     <Router>
@@ -15,6 +15,9 @@ function App() {
         <MovieListProvider>
           <Route exact path='/'>
             <MovieList />
+          </Route>
+          <Route path='/detail/:id'>
+            <MovieDetail />
           </Route>
         </MovieListProvider>
       </div>
---
+++ src/components/MovieDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import PosterLoader from './PosterLoader';
+
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_CDN_URL = 'https://image.tmdb.org/t/p/w440_and_h660_face/';
+
+export default function MovieDetail() {
+  return <div>캬캬</div>;
+}
