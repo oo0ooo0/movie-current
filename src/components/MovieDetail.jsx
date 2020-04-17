5e8251c007e7cec927ee443c855fae45d1d9b2f3
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-18 03:30:34 +0900

    add data to MovieDetail

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
-import PosterLoader from './PosterLoader';
 
 export const BASE_URL = 'https://api.themoviedb.org/3';
-export const IMAGE_CDN_URL = 'https://image.tmdb.org/t/p/w440_and_h660_face/';
 
 export default function MovieDetail() {
-  return <div>캬캬</div>;
+  const location = useLocation();
+  const [isFetching, setFetching] = React.useState(false);
+  const [reviews, setReviews] = React.useState([]);
+  const {
+    state: {
+      movie: {
+        popularity,
+        vote_count,
+        video,
+        poster_path,
+        id,
+        adult,
+        backdrop_path,
+        original_language,
+        original_title,
+
+        title,
+        vote_average,
+        overview,
+        release_date,
+      },
+    },
+  } = location;
+
+  return (
+    <div>
+      <div className='movieTitle'>
+        <span className='title'>{title}</span>
+        <span>{original_title}</span>
+      </div>
+
+      <p>평점: {vote_average}</p>
+      <p>{adult ? '청소년 관람 불가' : '청소년 관람 가능'}</p>
+      <p>출시 날짜: {release_date}</p>
+      <p>줄거리: {overview}</p>
+    </div>
+  );
 }
