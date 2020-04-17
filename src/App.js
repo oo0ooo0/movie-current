27c2844d0fc5aa64dfa900c1a1e08ba0306458e1
oo0ooo0 <nyanyanyangnyang@gmail.com>
2020-04-18 00:02:49 +0900

    add  Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyle';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyle />
       <div className='App'>!!!</div>
-    </>
+    </Router>
   );
 }
 
